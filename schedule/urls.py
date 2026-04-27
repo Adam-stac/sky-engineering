3a4ab292c7e8afbb80a90ce9a776d307f1704be3
Aryan Chandra <aryanchandra@Aryans-MacBook-Pro.local>
2026-04-27 16:48:27 +0530

    Initial commit - Django project with schedule feature

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'schedule'
-
 urlpatterns = [
+    path("", views.schedule_home, name="schedule_home"),
+    path("create/", views.create_meeting, name="create_meeting"),
+    path("delete/<int:meeting_id>/", views.delete_meeting, name="delete_meeting"),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Meeting
+from .forms import MeetingForm
+from django.utils.timezone import now
 
-# Create your views here.
+def schedule_home(request):
+    meetings = Meeting.objects.order_by("date", "time")
+    upcoming = Meeting.objects.filter(date__gte=now().date()).order_by("date")
+
+    return render(request, "schedule/schedule_home.html", {
+        "meetings": meetings,
+        "upcoming": upcoming
+    })
+
+
+def create_meeting(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("schedule_home")
+    else:
+        form = MeetingForm()
+
+    return render(request, "schedule/create_meeting.html", {"form": form})
+
+
+def delete_meeting(request, meeting_id):
+    meeting = get_object_or_404(Meeting, id=meeting_id)
+    meeting.delete()
+    return redirect("schedule_home")
